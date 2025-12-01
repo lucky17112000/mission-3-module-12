ddebceddb27b8c6b5a8d2a1011cec9ee217a855a
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2025-12-01 13:00:38 +0600

    post api with data insert with table

--- src/server.ts
+++ src/server.ts
@@ -21,28 +21,22 @@ const initDb = async () => {
         id SERIAL PRIMARY KEY,
         name  VARCHAR(100) NOT NULL,
         email VARCHAR(100) NOT NULL UNIQUE,
-        age INT NOT NULL,
+        age INT,
         phone VARCHAR(15),
         adress TEXT,
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
-
-
-        
-        
         )`);
 
   await pool.query(`CREATE TABLE IF NOT EXISTS todos(
-    
         id SERIAL PRIMARY KEY,
         user_id INT REFERENCES users(id) ON DELETE CASCADE,
-        title VARCHAR(255) NOT NULL,
+        title VARCHAR(255),
         description TEXT,
         is_completed BOOLEAN DEFAULT FALSE,
         due_date DATE,
         created_at TIMESTAMP DEFAULT NOW(),
         updated_at TIMESTAMP DEFAULT NOW()
-
     )`);
 };
 initDb();
@@ -50,12 +44,37 @@ initDb();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
 });
-app.post("/", (req: Request, res: Response) => {
-  // res.send("Post request received")
-  console.log(req.body);
-  res.status(201).json({ message: "Success True" });
+app.post("/users", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`,
+      [name, email]
+    );
+    // console.log(result.rows[0]);
+    res.status(201).json({
+      success: false,
+      message: "Data Instered Successfully",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    // console.log(err);
+
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+//package.json create kore server run kora
+// trype setup kora lagbe
+//tsconfig.json create kora lagbe
+//nodemon setup kora lagbe development er jonno
+//dotenv setup kora lagbe
+//pg setup kora lagbe database er jonno
