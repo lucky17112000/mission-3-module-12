fe9fb4cec36f6fa787457e165423359bc0a60fac
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2025-12-01 14:52:33 +0600

    x

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { Pool } from "pg";
 import dotenv from "dotenv";
 import path from "path";
@@ -41,7 +41,13 @@ const initDb = async () => {
 };
 initDb();
 
-app.get("/", (req: Request, res: Response) => {
+//logger middlware
+const loger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+  next();
+};
+
+app.get("/", loger, (req: Request, res: Response) => {
   res.send("Hello");
 });
 
@@ -86,7 +92,7 @@ app.get("/users", async (req: Request, res: Response) => {
     });
   }
 });
-
+//single user
 app.get("/users/:id", async (req: Request, res: Response) => {
   //catch id
   try {
@@ -111,6 +117,105 @@ app.get("/users/:id", async (req: Request, res: Response) => {
       message: err.message,
     });
   }
+});
+
+app.put("/users/:id", async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name=$1 , email=$2 WHERE id=$3 RETURNING *`,
+      [name, email, req.params.id]
+    );
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User updated  Successfully",
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      detailes: err,
+    });
+  }
+});
+
+app.delete("/users/:id", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`DELETE FROM users WHERE id=$1`, [
+      req.params.id,
+    ]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+        data: result.rows,
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+//todos crud
+
+app.post("/todos", async (req: Request, res: Response) => {
+  const { user_id, title } = req.body;
+  try {
+    const result = await pool.query(
+      `INSERT INTO todos(user_id , title)  VALUES($1 , $2 )  RETURNING * `,
+      [user_id, title]
+    );
+    res.status(201).json({
+      success: true,
+      message: "Todos created",
+      data: result.rows[0],
+    });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+});
+
+app.get("/todos", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`SELECT * FROM todos`);
+    res.status(200).json({
+      success: true,
+      message: "todos Retrived successfully",
+      data: result.rows,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+      detailes: err,
+    });
+  }
+});
+
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not found",
+    path: req.path,
+  });
 });
 
 app.listen(port, () => {
